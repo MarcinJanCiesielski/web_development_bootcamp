512a9708fc3f2f7568b44c7b856d8b14ee0a65e2
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2023-07-26 13:29:57 +0200

    Add S24.3 exercise

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName from "sillyname";
+import generateStupidName from "sillyname"; "superhero-name-library"
+
+let sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
+
+let herosName = generateStupidName();
+console.log("I am " + herosName + "!")
