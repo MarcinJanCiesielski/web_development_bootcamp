5cbe4abcf33cabdaaf41aaa0dd87b125f0b359b9
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2023-07-28 18:17:16 +0200

    Add S25.2 exercise

--- 25.2 Passing Data/index.js
+++ 25.2 Passing Data/index.js
@@ -4,11 +4,19 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let greetings = "Enter your name, please"
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs", {greetings : greetings})
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const nameLength = req.body["fName"].length + req.body["lName"].length;
+  greetings = "There are " + nameLength + " letters in your name";
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
