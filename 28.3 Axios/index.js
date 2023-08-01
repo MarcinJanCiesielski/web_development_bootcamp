0e4ee9e850e42e57535fc4c3a4670266c4f0dccf
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2023-08-01 13:07:40 +0200

    Add S28.3 exercise

--- 28.3 Axios/index.js
+++ 28.3 Axios/index.js
@@ -8,9 +8,6 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
@@ -18,25 +15,41 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
+    res.render("index.ejs", {error: error.message});
   }
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  const request = buildRequest(req.body);
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/filter" + request);
+    let result = response.data;
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+    if (result.length > 1) {
+      result = result[Math.floor(Math.random() * result.length)]
+    }
+
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {error: error.message});
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+
+function buildRequest(body) {
+  let request = '';
+
+  if (body.type.length > 0 || body.participants.length > 0) {
+    request = "?"
+    if (body.type.length > 0) request = request + "type=" + body.type;
+    if (body.participants.length > 0) request = request + "&participants=" + body.participants;
+    
+    console.log(request);
+  }
+  return request;
+}
