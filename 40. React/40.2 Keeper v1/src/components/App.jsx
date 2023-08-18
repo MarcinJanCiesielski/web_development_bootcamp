01754a7d394a0039e7d0eccdbe2d74f19d204088
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2023-08-18 17:16:18 +0200

    Add S40.2 Keeper v1

---
+++ 40. React/40.2 Keeper v1/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Keeper from "./Keeper";
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Keeper></Keeper>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ 40. React/40.2 Keeper v1/src/components/Keeper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Note from "./Note";
+
+function Keeper() {
+  return (
+    <Note></Note>
+  );
+};
+
+export default Keeper;
